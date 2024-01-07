73fc87295b6171cc4a7d3f8d115dcc1a67aae165
allen spayne <mainacc.salapayne@gmail.com>
2024-01-07 11:39:02 +0800

    first commit

---
+++ components/shared/RightSideBar.tsx
@@ -0,0 +1,13 @@
+function RightSideBar() {
+    return(
+        <section className="custom-scrollbar rightsidebar">
+            <div className="flex flex-1 flex-col justify-start">
+                <h3>
+                    suggested communities
+                </h3>
+            </div>
+        </section>
+    )
+}
+
+export default RightSideBar;
